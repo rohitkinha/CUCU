0dc31955a1cbe9fa2ac58111afd457a8d1c14bcd
rohitkinha <87006871+rohitkinha@users.noreply.github.com>
2022-06-30 16:16:25 +0530

    Add files via upload

---
+++ Sample1.cu
@@ -0,0 +1,29 @@
+int a;
+int i = 0+4;
+a = i + 3;
+/*dfsd
+ddfsdfwrerwrt
+qewqwe
+*/
+int add(int x, int y);
+int subtract(int q,int w,char *s){
+    int i;
+    add(1, 2);
+    i = 2 + 3;
+    return add(2, 4);
+    if(a>2+d)
+    {
+        q = 2 + 3 + (1 - 2) + 3 * 7;
+
+
+        return;
+    }
+    else
+    {
+        int j = 1;
+    }
+    while(i>10)
+    {
+        return add(a, 10);
+    }
+}
